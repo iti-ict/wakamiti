dcbd573c63b18dd0141ca82222538b67af126aaf
mgalbis <mgalbis@iti.es>
2020-06-16 23:30:35 +0200

    fix: escaping special characters to correctly parse steps

--- kukumo/kukumo-core/src/main/java/iti/kukumo/core/backend/ExpressionMatcher.java
+++ kukumo/kukumo-core/src/main/java/iti/kukumo/core/backend/ExpressionMatcher.java
@@ -84,9 +84,9 @@ public class ExpressionMatcher {
         // (( -> ( and )) -> (
         regex = regex.replace("((", "(").replace("))", ")");
         // * -> any value
-        regex = regex.replaceAll("(?<!/)\\*", "(.*)");
+        regex = regex.replaceAll("(?<!\\\\)\\*", "(.*)");
         // ( ) -> optional
-        regex = regex.replaceAll("(?<!/)\\(([^)]*)\\)", "(?:$1)?");
+        regex = regex.replaceAll("(?<!\\\\)\\(([^)]*)\\)", "(?:$1)?");
         // (...)?_ -> (?:(...)?_)?
         regex = regex.replaceAll("\\(\\?:[^)]+\\)\\? ", "(?:$0)?");
         // _(?:.*)? -> (?:.*)?
