2f3b16b330a98e693110c23870b55f416d5cd2a6
luis <linesta@iti.es>
2021-02-25 18:40:21 +0100

    vscode extension

---
+++ kukumo/kukumo-vscode-extension/src/extension.ts
@@ -0,0 +1,18 @@
+import * as vscode from 'vscode';
+import { startLanguageClient } from './language-client';
+
+
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Activating Kukumo VSCode extension...');
+	startLanguageClient(context);	
+	console.log('Kukumo VSCode extension activated.');
+}
+
+
+export function deactivate() {
+	console.log('Kukumo VSCode extension deactivated.');
+}
+
+
+
