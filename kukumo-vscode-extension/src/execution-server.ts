1df53e81b7e0141e6c4ea5ae34684da7b652b2a0
luis <linesta@iti.es>
2022-03-21 14:33:36 +0100

    updated LICENSE in vscode-extension

--- kukumo-vscode-extension/src/execution-server.ts
+++ kukumo-vscode-extension/src/execution-server.ts
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 import * as settings from './shared/settings';
 import * as http from './shared/http-client';
 import * as vscode from 'vscode';
--- kukumo-vscode-extension/src/model/PlanNodeSnapshot.ts
+++ kukumo-vscode-extension/src/model/PlanNodeSnapshot.ts
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 export class PlanNodeSnapshot {
 
     executionID: string | undefined;
@@ -25,4 +30,3 @@ export class PlanNodeSnapshot {
     children: PlanNodeSnapshot[] | undefined;
 
 }
-
