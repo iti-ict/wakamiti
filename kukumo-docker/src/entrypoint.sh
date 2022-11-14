a6c41e989e612859f15ac04ceaf050f678a6ac36
mgalbis <mgalbis@iti.es>
2022-11-14 20:41:27 +0100

    remove env properties

--- kukumo-docker/src/entrypoint.sh
+++ kukumo-docker/src/entrypoint.sh
@@ -6,27 +6,6 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #
 
-shopt -s nullglob
-replace_properties() {
-  cur_dir="$1"
-  for file in $cur_dir/*.properties; do
-    bck=/tmp/${file////_}.bck
-    mv $file $bck
-    eval "echo \"$(cat $bck)\"" > $file
-  done
-}
-
-revert_replace_properties() {
-  cur_dir="$1"
-  for file in $cur_dir/*.properties; do
-    bck=/tmp/${file////_}.bck
-    rm -rf $file
-    mv $bck $file
-  done
-}
-
-replace_properties $KUKUMO_HOME
-
 # collect arguments
 args=()
 for i in "$@"; do
@@ -41,7 +20,5 @@ done
 echo Executing at $(date): kukumo ${args[@]}
 kukumo "$@"
 status=$?
-
-revert_replace_properties $KUKUMO_HOME
 
 exit $status
