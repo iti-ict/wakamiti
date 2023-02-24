7fe5b8a81e570a62c17eaf330b2161b5d20438ed
mgalbis <mgalbis@iti.es>
2023-02-24 08:55:04 +0100

    fix: issue when no children

--- kukumo/kukumo-api/src/main/java/iti/kukumo/api/plan/PlanNodeSnapshot.java
+++ kukumo/kukumo-api/src/main/java/iti/kukumo/api/plan/PlanNodeSnapshot.java
@@ -3,12 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-
-/**
- * @author Luis Iñesta Gelabert - linesta@iti.es | luiinge@gmail.com
- */
 package iti.kukumo.api.plan;
-
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -25,7 +20,13 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 
-/** This class is a immutable, non-executable representation of a * {@link PlanNode} in a specific state. * <p> * It is mainly used for serialization/deserialization operations. * </p> */
+/**
+ * This class is an immutable, non-executable representation of a
+ * {@link PlanNode} in a specific state. It is mainly used for
+ * serialization/deserialization operations.
+ *
+ * @author Luis Iñesta Gelabert - linesta@iti.es | luiinge@gmail.com
+ */
 public class PlanNodeSnapshot {
 
     private String executionID;
@@ -61,7 +62,7 @@ public class PlanNodeSnapshot {
         this(node, LocalDateTime.now().toString());
     }
 
-    
+
     public PlanNodeSnapshot(PlanNode node, String snapshotInstant) {
         this.executionID = node.executionID();
         this.snapshotInstant = snapshotInstant;
@@ -72,76 +73,29 @@ public class PlanNodeSnapshot {
         this.language = node.language();
         this.source = node.source();
         this.displayName = node.displayName();
-        this.description = (node.description() == null ? null
-                        : new ArrayList<>(node.description()));
-        this.tags = (node.tags() == null ? null : new ArrayList<>(node.tags()));
-        this.properties = (node.properties() == null ? null
-                        : new LinkedHashMap<>(node.properties()));
+        this.description = new LinkedList<>(node.description() == null ? List.of() : node.description());
+        this.tags = new LinkedList<>(node.tags() == null ? List.of() : node.tags());
+        this.properties = new LinkedHashMap<>(node.properties() == null ? Map.of() : node.properties());
         this.startInstant = node.startInstant().map(this::instantToString).orElse(null);
         this.finishInstant = node.finishInstant().map(this::instantToString).orElse(null);
         this.duration = node.duration().map(Duration::toMillis).orElse(null);
         this.result = node.result().orElse(null);
         this.document = node.data().filter(Document.class::isInstance).map(Document.class::cast)
-            .map(Document::getContent).orElse(null);
+                .map(Document::getContent).orElse(null);
         this.documentType = node.data().filter(Document.class::isInstance).map(Document.class::cast)
-            .map(Document::getContentType).orElse(null);
+                .map(Document::getContentType).orElse(null);
         this.dataTable = node.data().filter(DataTable.class::isInstance).map(DataTable.class::cast)
-            .map(DataTable::getValues).orElse(null);
+                .map(DataTable::getValues).orElse(null);
         this.errorMessage = node.errors().findFirst().map(Throwable::getLocalizedMessage)
-            .orElse(null);
+                .orElse(null);
         this.errorTrace = node.errors().findFirst().map(this::errorTrace).orElse(null);
         if (node.hasChildren()) {
-            this.children = node.children().map(child -> new PlanNodeSnapshot(child,snapshotInstant))
-                .collect(Collectors.toList());
+            this.children = node.children().map(child -> new PlanNodeSnapshot(child, snapshotInstant))
+                    .collect(Collectors.toList());
             this.testCaseResults = countTestCases(node);
             this.childrenResults = countChildren(node);
         }
     }
-
-
-
-    public PlanNodeSnapshot withoutChildren() {
-        PlanNodeSnapshot copy = new PlanNodeSnapshot();
-        copy.executionID = this.executionID;
-        copy.snapshotInstant = this.snapshotInstant;
-        copy.nodeType = this.nodeType;
-        copy.id = this.id;
-        copy.name = this.name;
-        copy.keyword = this.keyword;
-        copy.language = this.language;
-        copy.source = this.source;
-        copy.displayName = this.displayName;
-        copy.description = this.description;
-        copy.tags = new ArrayList<>(this.tags);
-        copy.properties = new LinkedHashMap<>(this.properties);
-        copy.startInstant = this.startInstant;
-        copy.finishInstant = this.finishInstant;
-        copy.duration = this.duration;
-        copy.result = this.result;
-        copy.document = this.document;
-        copy.documentType = this.documentType;
-        copy.dataTable = this.dataTable;
-        copy.errorMessage = this.errorMessage;
-        copy.errorTrace = this.errorTrace;
-        copy.testCaseResults = new LinkedHashMap<>(this.testCaseResults);
-        copy.childrenResults = new LinkedHashMap<>(this.childrenResults);
-        return copy;
-    }
-
-
-
-
-    private String instantToString(Instant instant) {
-        return LocalDateTime.ofInstant(instant, ZoneOffset.UTC).toString();
-    }
-
-
-    private String errorTrace(Throwable error) {
-        StringWriter errorWriter = new StringWriter();
-        error.printStackTrace(new PrintWriter(errorWriter));
-        return errorWriter.toString();
-    }
-
 
     /**
      * Creates a new node descriptor as a parent of the specified nodes
@@ -156,30 +110,29 @@ public class PlanNodeSnapshot {
         PlanNodeSnapshot root = new PlanNodeSnapshot();
         root.children = Arrays.asList(nodes);
         root.startInstant = childLocalDateTime(
-            root,
-            PlanNodeSnapshot::getStartInstant,
-            (x, y) -> x.compareTo(y) < 0 ? x : y
+                root,
+                PlanNodeSnapshot::getStartInstant,
+                (x, y) -> x.compareTo(y) < 0 ? x : y
         );
         root.finishInstant = childLocalDateTime(
-            root,
-            PlanNodeSnapshot::getFinishInstant,
-            (x, y) -> x.compareTo(y) > 0 ? x : y
+                root,
+                PlanNodeSnapshot::getFinishInstant,
+                (x, y) -> x.compareTo(y) > 0 ? x : y
         );
         root.duration = maxChild(root, PlanNodeSnapshot::getDuration);
         root.result = maxChild(root, PlanNodeSnapshot::getResult);
         root.testCaseResults = new LinkedHashMap<>();
         root.children.stream().map(PlanNodeSnapshot::getTestCaseResults).filter(Objects::nonNull)
-            .flatMap(map -> map.entrySet().stream()).forEach(entry -> {
-                root.testCaseResults.computeIfAbsent(entry.getKey(), x -> 0L);
-                root.testCaseResults.put(
-                    entry.getKey(),
-                    root.testCaseResults.get(entry.getKey()) + entry.getValue()
-                );
-            });
+                .flatMap(map -> map.entrySet().stream()).forEach(entry -> {
+                    root.testCaseResults.computeIfAbsent(entry.getKey(), x -> 0L);
+                    root.testCaseResults.put(
+                            entry.getKey(),
+                            root.testCaseResults.get(entry.getKey()) + entry.getValue()
+                    );
+                });
         root.childrenResults = countChildren(root);
         return root;
     }
-
 
     private static Map<Result, Long> countTestCases(PlanNode node) {
         LinkedHashMap<Result, Long> results = new LinkedHashMap<>();
@@ -187,47 +140,80 @@ public class PlanNodeSnapshot {
             node.result().ifPresent(testCaseResult -> results.put(testCaseResult, 1L));
         } else if (node.hasChildren()) {
             node.children().map(PlanNodeSnapshot::countTestCases).filter(Objects::nonNull)
-                .flatMap(map -> map.entrySet().stream())
-                .forEach(entry -> {
-                    results.computeIfAbsent(entry.getKey(), x -> 0L);
-                    results.put(entry.getKey(), results.get(entry.getKey()) + entry.getValue());
-                });
+                    .flatMap(map -> map.entrySet().stream())
+                    .forEach(entry -> {
+                        results.computeIfAbsent(entry.getKey(), x -> 0L);
+                        results.put(entry.getKey(), results.get(entry.getKey()) + entry.getValue());
+                    });
         }
         return results;
     }
 
-
     private static Map<Result, Long> countChildren(PlanNode node) {
         return node.children()
-            .filter(it->it.result().isPresent())
-            .collect(groupingBy(it->it.result().orElseThrow(), counting()));
+                .filter(it -> it.result().isPresent())
+                .collect(groupingBy(it -> it.result().orElseThrow(), counting()));
     }
-
 
     private static Map<Result, Long> countChildren(PlanNodeSnapshot node) {
-        return node.getChildren().stream().collect(groupingBy(it->it.getResult(), counting()));
+        return node.getChildren().stream().collect(groupingBy(it -> it.getResult(), counting()));
     }
-
 
     private static String childLocalDateTime(
-        PlanNodeSnapshot node,
-        Function<PlanNodeSnapshot, String> method,
-        BinaryOperator<LocalDateTime> reductor
+            PlanNodeSnapshot node,
+            Function<PlanNodeSnapshot, String> method,
+            BinaryOperator<LocalDateTime> reductor
     ) {
         return node.children.stream().map(method).filter(Objects::nonNull).map(LocalDateTime::parse)
-            .reduce(reductor)
-            .map(LocalDateTime::toString).orElse(null);
+                .reduce(reductor)
+                .map(LocalDateTime::toString).orElse(null);
     }
-
 
     private static <T extends Comparable<T>> T maxChild(
-        PlanNodeSnapshot node,
-        Function<PlanNodeSnapshot, T> mapper
+            PlanNodeSnapshot node,
+            Function<PlanNodeSnapshot, T> mapper
     ) {
         return node.children.stream().map(mapper).filter(Objects::nonNull)
-            .max(Comparator.naturalOrder()).orElse(null);
+                .max(Comparator.naturalOrder()).orElse(null);
     }
 
+    public PlanNodeSnapshot withoutChildren() {
+        PlanNodeSnapshot copy = new PlanNodeSnapshot();
+        copy.executionID = this.executionID;
+        copy.snapshotInstant = this.snapshotInstant;
+        copy.nodeType = this.nodeType;
+        copy.id = this.id;
+        copy.name = this.name;
+        copy.keyword = this.keyword;
+        copy.language = this.language;
+        copy.source = this.source;
+        copy.displayName = this.displayName;
+        copy.description = this.description;
+        copy.tags = this.tags;
+        copy.properties = this.properties;
+        copy.startInstant = this.startInstant;
+        copy.finishInstant = this.finishInstant;
+        copy.duration = this.duration;
+        copy.result = this.result;
+        copy.document = this.document;
+        copy.documentType = this.documentType;
+        copy.dataTable = this.dataTable;
+        copy.errorMessage = this.errorMessage;
+        copy.errorTrace = this.errorTrace;
+        copy.testCaseResults = this.testCaseResults;
+        copy.childrenResults = this.childrenResults;
+        return copy;
+    }
+
+    private String instantToString(Instant instant) {
+        return LocalDateTime.ofInstant(instant, ZoneOffset.UTC).toString();
+    }
+
+    private String errorTrace(Throwable error) {
+        StringWriter errorWriter = new StringWriter();
+        error.printStackTrace(new PrintWriter(errorWriter));
+        return errorWriter.toString();
+    }
 
     public NodeType getNodeType() {
         return nodeType;
