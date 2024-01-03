f36944262ad5caf6b60e4a6194001f119ad32b12
luis <linesta@iti.es>
2024-01-03 14:03:32 +0100

    fix: evil regex

--- plugins/azure-wakamiti-plugin/src/test/java/es/iti/wakamiti/azure/TestUtil.java
+++ plugins/azure-wakamiti-plugin/src/test/java/es/iti/wakamiti/azure/TestUtil.java
@@ -9,9 +9,16 @@ public class TestUtil {
 
 
     @Test
-    public void testParseNameAndId() {
+    public void testParseNameWithoutId() {
         Pair<String, String> parsed = Util.parseNameAndId("Wakamiti test plan");
         Assert.assertTrue(parsed.key().equals("Wakamiti test plan"));
         Assert.assertTrue(parsed.value() == null);
+    }
+
+    @Test
+    public void testParseNameWithId() {
+        Pair<String, String> parsed = Util.parseNameAndId("[423423] Wakamiti test plan");
+        Assert.assertTrue(parsed.key().equals("Wakamiti test plan"));
+        Assert.assertTrue(parsed.value().equals("423423"));
     }
 }
