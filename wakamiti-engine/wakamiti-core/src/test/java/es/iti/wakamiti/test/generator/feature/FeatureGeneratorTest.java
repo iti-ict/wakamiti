41628cc5dea462f871058c7f6a891d3ff87a0e86
Maria Galbis <mgalbis@iti.es>
2024-09-26 11:32:07 +0200

    test: add feature generator unit tests

--- wakamiti-engine/wakamiti-core/src/test/java/es/iti/wakamiti/test/generator/feature/FeatureGeneratorTest.java
+++ wakamiti-engine/wakamiti-core/src/test/java/es/iti/wakamiti/test/generator/feature/FeatureGeneratorTest.java
@@ -1,42 +1,187 @@
 package es.iti.wakamiti.test.generator.feature;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import es.iti.wakamiti.api.util.WakamitiLogger;
 import es.iti.wakamiti.core.generator.features.FeatureGenerator;
+import es.iti.wakamiti.core.generator.features.FeatureGeneratorException;
 import es.iti.wakamiti.core.generator.features.OpenAIService;
+import org.apache.commons.io.IOUtils;
+import org.assertj.core.util.Files;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockserver.integration.ClientAndServer;
+import org.mockserver.matchers.Times;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.MediaType;
+import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import static org.mockserver.model.HttpRequest.request;
+import static org.mockserver.model.HttpResponse.response;
 
 @RunWith(MockitoJUnitRunner.Silent.class)
 public class FeatureGeneratorTest {
 
-    private static final String API_DOCS_JSON = "{\"openapi\":\"3.0.1\",\"info\":{\"title\":\"Notification Service Api\",\"description\":\"Notification Service Api for Dataspace\",\"termsOfService\":\"https://www.iti.es/nota-legal/\",\"contact\":{\"name\":\"ITI\",\"url\":\"https://www.iti.es\",\"email\":\"dataspace@iti.es\"},\"license\":{\"name\":\"LICENSE Apache 2.0\",\"url\":\"https://www.apache.org/licenses/LICENSE-2.0\"},\"version\":\"0.0.1\"},\"servers\":[{\"url\":\"https://pre-backend.dataspace.iti.es/notification-service\",\"description\":\"Docker local\"}],\"paths\":{\"/notifications/{id}\":{\"get\":{\"tags\":[\"Notifications\"],\"summary\":\"Get notification details\",\"description\":\"Get notification including the status history\",\"operationId\":\"getNotificationDetails\",\"parameters\":[{\"name\":\"id\",\"in\":\"path\",\"required\":true,\"schema\":{\"type\":\"string\",\"format\":\"uuid\"}}],\"responses\":{\"200\":{\"description\":\"OK\",\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/NotificationDetailsDto\"}}}}},\"security\":[{\"bearerAuth\":[]}]},\"put\":{\"tags\":[\"Notifications\"],\"summary\":\"Update notification\",\"description\":\"Update a notification\",\"operationId\":\"updateNotification\",\"parameters\":[{\"name\":\"id\",\"in\":\"path\",\"required\":true,\"schema\":{\"type\":\"string\",\"format\":\"uuid\"}}],\"requestBody\":{\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/UpdateNotificationDto\"}}},\"required\":true},\"responses\":{\"200\":{\"description\":\"The notification just updated\",\"content\":{\"application/json\":{\"schema\":{\"type\":\"array\",\"items\":{\"$ref\":\"#/components/schemas/NotificationDto\"}}}}}},\"security\":[{\"bearerAuth\":[]}]}},\"/notifications\":{\"post\":{\"tags\":[\"Notifications\"],\"summary\":\"Create notification\",\"description\":\"Create multiple types of notifications\",\"operationId\":\"createNotification\",\"requestBody\":{\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/CreateNotificationDto\"}}},\"required\":true},\"responses\":{\"201\":{\"description\":\"The notification just created\",\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/NotificationDto\"}}}}},\"security\":[{\"bearerAuth\":[]}]}},\"/notifications/reasons\":{\"get\":{\"tags\":[\"Notifications\"],\"summary\":\"Get all notification reasons\",\"description\":\"Get all notification reasons\",\"operationId\":\"getAllNotificationReasons\",\"responses\":{\"200\":{\"description\":\"List containing all reasons available for the notifications\",\"content\":{\"application/json\":{\"schema\":{\"type\":\"array\",\"items\":{\"$ref\":\"#/components/schemas/NotificationReasonDto\"}}}}}},\"security\":[{\"bearerAuth\":[]}]}},\"/notifications/personal\":{\"get\":{\"tags\":[\"Notifications\"],\"summary\":\"Get personal notifications\",\"description\":\"Get the notifications assigned to the currently authenticated user\",\"operationId\":\"getPersonalNotifications\",\"responses\":{\"200\":{\"description\":\"List of notifications\",\"content\":{\"application/json\":{\"schema\":{\"type\":\"array\",\"items\":{\"$ref\":\"#/components/schemas/NotificationDto\"}}}}}},\"security\":[{\"bearerAuth\":[]}]}},\"/notifications/kinds\":{\"get\":{\"tags\":[\"Notifications\"],\"summary\":\"Get all notification kinds\",\"description\":\"Get all notification kinds\",\"operationId\":\"getAllNotificationKinds\",\"responses\":{\"200\":{\"description\":\"List containing all kinds of notifications available\",\"content\":{\"application/json\":{\"schema\":{\"type\":\"array\",\"items\":{\"$ref\":\"#/components/schemas/NotificationKindDto\"}}}}}},\"security\":[{\"bearerAuth\":[]}]}},\"/health\":{\"get\":{\"tags\":[\"Health\"],\"summary\":\"Check for service healthiness\",\"description\":\"Endpoint to check for API healthiness\",\"operationId\":\"getHealth\",\"responses\":{\"200\":{\"description\":\"Returns ok if server is in a healthy state\",\"content\":{\"application/json\":{\"schema\":{\"$ref\":\"#/components/schemas/HealthDto\"}}}}}}}},\"components\":{\"schemas\":{\"NotificationDto\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"string\",\"format\":\"uuid\"},\"fromUser\":{\"type\":\"string\",\"format\":\"uuid\"},\"toUser\":{\"type\":\"string\"},\"message\":{\"type\":\"string\"},\"kind\":{\"type\":\"string\",\"enum\":[\"LOG\",\"MSG\"]},\"reason\":{\"type\":\"string\",\"enum\":[\"ORG_CREATED\",\"INVITATION_SEND\",\"USER_REGISTERED\",\"USER_ASSIGNED\",\"DATASET_CREATED\",\"ARTIFACT_UPLOADED\",\"ARTIFACT_PROFILED\",\"ARTIFACT_PROFILING_ERROR\"]},\"generationDate\":{\"type\":\"string\",\"format\":\"date-time\"},\"favorite\":{\"type\":\"boolean\"}}},\"UpdateNotificationDto\":{\"type\":\"object\",\"properties\":{\"read\":{\"type\":\"string\",\"enum\":[\"READ\",\"UNREAD\"]},\"favorite\":{\"type\":\"boolean\"}}},\"CreateNotificationDto\":{\"required\":[\"fromUser\",\"kind\",\"message\",\"reason\",\"toUser\"],\"type\":\"object\",\"properties\":{\"kind\":{\"type\":\"string\",\"enum\":[\"LOG\",\"MSG\"]},\"reason\":{\"type\":\"string\",\"enum\":[\"ORG_CREATED\",\"INVITATION_SEND\",\"USER_REGISTERED\",\"USER_ASSIGNED\",\"DATASET_CREATED\",\"ARTIFACT_UPLOADED\",\"ARTIFACT_PROFILED\",\"ARTIFACT_PROFILING_ERROR\"]},\"fromUser\":{\"type\":\"string\",\"format\":\"uuid\"},\"toUser\":{\"type\":\"string\"},\"message\":{\"type\":\"string\"},\"generationDate\":{\"type\":\"string\",\"format\":\"date-time\"}}},\"NotificationDetailsDto\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"string\",\"format\":\"uuid\"},\"fromUser\":{\"type\":\"string\",\"format\":\"uuid\"},\"toUser\":{\"type\":\"string\"},\"message\":{\"type\":\"string\"},\"kind\":{\"type\":\"string\",\"enum\":[\"LOG\",\"MSG\"]},\"reason\":{\"type\":\"string\",\"enum\":[\"ORG_CREATED\",\"INVITATION_SEND\",\"USER_REGISTERED\",\"USER_ASSIGNED\",\"DATASET_CREATED\",\"ARTIFACT_UPLOADED\",\"ARTIFACT_PROFILED\",\"ARTIFACT_PROFILING_ERROR\"]},\"generationDate\":{\"type\":\"string\",\"format\":\"date-time\"},\"favorite\":{\"type\":\"boolean\"},\"notificationStatusHistory\":{\"type\":\"array\",\"items\":{\"$ref\":\"#/components/schemas/NotificationStatusDto\"}}}},\"NotificationStatusDto\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"string\",\"format\":\"uuid\"},\"status\":{\"type\":\"string\",\"enum\":[\"READ\",\"UNREAD\",\"SENT\",\"ERROR\",\"RETRIED\"]},\"message\":{\"type\":\"string\"},\"timestamp\":{\"type\":\"string\",\"format\":\"date-time\"}}},\"NotificationReasonDto\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"integer\",\"format\":\"int32\"},\"name\":{\"type\":\"string\"}}},\"NotificationKindDto\":{\"type\":\"object\",\"properties\":{\"id\":{\"type\":\"integer\",\"format\":\"int32\"},\"name\":{\"type\":\"string\"}}},\"HealthDto\":{\"type\":\"object\",\"properties\":{\"healthy\":{\"type\":\"boolean\"}}}},\"securitySchemes\":{\"bearerAuth\":{\"type\":\"http\",\"scheme\":\"bearer\",\"bearerFormat\":\"JWT\"}}}}\n";
-    private static final String TEMP_PATH = "/var/tmp";
-    private static final String GENERATED_FILE = "/var/tmp/get_health.feature";
+    private static final Logger LOGGER = WakamitiLogger.forClass(FeatureGeneratorTest.class);
+
+    private static final String API_DOCS_JSON = "api_docs.json";
+    private static final String TEMP_PATH = Files.temporaryFolderPath();
+    private static final Integer PORT = 4321;
+    private static final String BASE_URL = MessageFormat.format("http://localhost:{0}", PORT);
+    private static final ClientAndServer server = startClientAndServer(PORT);
+    private static final List<File> FEATURES = Stream.of(
+                    "get_health", "post_notifications", "get_notifications_{id}", "put_notifications_{id}",
+                    "get_notifications_kinds", "get_notifications_personal", "get_notifications_reasons")
+            .map(f -> Path.of(TEMP_PATH, f + ".feature").toFile())
+            .collect(Collectors.toList());
 
     @Mock
     private OpenAIService openAIService;
 
+    @AfterClass
+    public static void shutdown() {
+        server.close();
+    }
+
+    @Before
+    public void beforeEach() throws URISyntaxException, JsonProcessingException {
+        when(openAIService.runPrompt(anyString(), anyString())).thenReturn("Something");
+        server.reset();
+    }
+
+    @After
+    public void afterEach() {
+        FEATURES.forEach(File::delete);
+    }
+
     @Test
-    public void shouldGenerateFeatures() throws URISyntaxException, IOException {
+    public void testGenerateTestWhenContentWithSuccess() throws URISyntaxException, IOException {
+        generateTest(content(API_DOCS_JSON));
+    }
 
-        FeatureGenerator featureGenerator = new FeatureGenerator(openAIService, "", API_DOCS_JSON);
+    @Test(expected = FeatureGeneratorException.class)
+    public void testGenerateTestWhenWrongContentWithError() {
+        try {
+            generateTest("ghewpiog");
+        } catch (URISyntaxException | IOException e) {
+            throw new FeatureGeneratorException("Controlled exception", e);
+        } catch (Exception e) {
+            LOGGER.error("Uncontrolled exception");
+            throw e;
+        }
+    }
 
-        when(openAIService.runPrompt(anyString(), anyString())).thenReturn("");
+    @Test
+    public void testGenerateTestWhenLocalURLWithSuccess() throws URISyntaxException, IOException {
+        generateTest(url(API_DOCS_JSON).getFile());
+    }
 
+    @Test(expected = FeatureGeneratorException.class)
+    public void testGenerateTestWhenWrongLocalURLWithError() {
+        try {
+            generateTest(url("simplelogger.properties").getFile());
+        } catch (URISyntaxException | IOException e) {
+            throw new FeatureGeneratorException("Controlled exception", e);
+        } catch (Exception e) {
+            LOGGER.error("Uncontrolled exception");
+            throw e;
+        }
+    }
+
+    @Test(expected = FeatureGeneratorException.class)
+    public void testGenerateTestWhenNonExistentLocalURLWithError() {
+        try {
+            generateTest("somefile.json");
+        } catch (URISyntaxException | IOException e) {
+            throw new FeatureGeneratorException("Controlled exception", e);
+        } catch (Exception e) {
+            LOGGER.error("Uncontrolled exception");
+            throw e;
+        }
+    }
+
+    @Test
+    public void testGenerateTestWhenHttpURLWithSuccess() throws URISyntaxException, IOException {
+        mockServer(
+                request()
+                        .withMethod("GET")
+                        .withPath("/api_docs")
+                ,
+                response()
+                        .withStatusCode(200)
+                        .withContentType(MediaType.APPLICATION_JSON)
+                        .withBody(content(API_DOCS_JSON))
+        );
+
+        generateTest(BASE_URL + "/api_docs");
+    }
+
+    @Test(expected = FeatureGeneratorException.class)
+    public void testGenerateTestWhenWrongHttpURLWithError() {
+        mockServer(
+                request()
+                        .withMethod("GET")
+                        .withPath("/api_docs")
+                ,
+                response()
+                        .withStatusCode(404)
+                        .withContentType(MediaType.APPLICATION_JSON)
+        );
+
+        try {
+            generateTest(BASE_URL + "/api_docs");
+        } catch (URISyntaxException | IOException e) {
+            throw new FeatureGeneratorException("Controlled exception", e);
+        } catch (Exception e) {
+            LOGGER.error("Uncontrolled exception");
+            throw e;
+        }
+    }
+
+    private void generateTest(String file) throws URISyntaxException, IOException {
+        FeatureGenerator featureGenerator = new FeatureGenerator(openAIService, "", file);
         featureGenerator.generate(TEMP_PATH);
 
-        File file = new File(GENERATED_FILE);
-        assertTrue(file.exists());
+        assertThat(FEATURES).allMatch(File::exists)
+                .allMatch(f -> Files.contentOf(f, Charset.defaultCharset()).equals("Something"));
 
+    }
+
+    private URL url(String resource) {
+        return Objects.requireNonNull(this.getClass().getClassLoader().getResource(resource));
+    }
+
+    private String content(String resource) throws IOException {
+        return IOUtils.toString(url(resource), Charset.defaultCharset());
+    }
+
+    private void mockServer(HttpRequest expected, HttpResponse response) {
+        mockServer(expected, response, Times.once());
+    }
+
+    private void mockServer(HttpRequest expected, HttpResponse response, Times times) {
+        server.when(expected, times).respond(response);
     }
 
 }
