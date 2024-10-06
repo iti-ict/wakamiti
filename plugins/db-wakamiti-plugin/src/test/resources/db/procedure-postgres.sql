1af5cee2782b3319b1558c30b65367cc33b04e89
Maria Galbis <mgalbis@iti.es>
2024-10-06 17:19:58 +0200

    fix(database): Boolean columns issues

--- plugins/db-wakamiti-plugin/src/test/resources/db/procedure-postgres.sql
+++ plugins/db-wakamiti-plugin/src/test/resources/db/procedure-postgres.sql
@@ -2,6 +2,6 @@
 INSERT INTO client (id, first_name, second_name, birth_date) VALUES (2, 'Ester', 'Colero', '2000-01-02');
 INSERT INTO city (id, name, latitude, longitude) SELECT 2, 'Madrid', 40.416775, -3.703790;
 SELECT * FROM client ;
-UPDATE client SET active = 0 WHERE id = 1;
+UPDATE client SET active = false WHERE id = 1;
 INSERT INTO client_city ("clientId", "cityId") VALUES (2, 2);
 INSERT INTO client_city ("clientId", "cityId") VALUES (2, 1);
