e1192c838529b7dc3fe3e6ba683d147f0020c806
coliva <coliva@iti.es>
2024-12-03 12:54:52 +0100

    feat: add test execution

--- plugins/xray-wakamiti-plugin/src/main/java/es/iti/wakamiti/xray/internal/ScenarioMapper.java
+++ plugins/xray-wakamiti-plugin/src/main/java/es/iti/wakamiti/xray/internal/ScenarioMapper.java
@@ -4,7 +4,7 @@ import es.iti.wakamiti.api.plan.PlanNodeSnapshot;
 import es.iti.wakamiti.api.util.Pair;
 import es.iti.wakamiti.xray.XRaySynchronizer;
 import es.iti.wakamiti.xray.model.JiraIssue;
-import es.iti.wakamiti.xray.model.XRayTestSet;
+import es.iti.wakamiti.xray.model.TestSet;
 
 import java.util.stream.Stream;
 
@@ -16,11 +16,11 @@ public class ScenarioMapper extends Mapper {
     }
 
     @Override
-    protected Stream<Pair<PlanNodeSnapshot, XRayTestSet>> suiteMap(PlanNodeSnapshot target) {
+    protected Stream<Pair<PlanNodeSnapshot, TestSet>> suiteMap(PlanNodeSnapshot target) {
         return super.suiteMap(target)
                 .flatMap(p ->
                         p.key().flatten(node -> gherkinType(node).equals(type()))
-                                .map(node -> new Pair<>(node, new XRayTestSet().issue(new JiraIssue().summary(p.key().getName()))))
+                                .map(node -> new Pair<>(node, new TestSet().issue(new JiraIssue().summary(p.key().getName()))))
                 );
     }
 
