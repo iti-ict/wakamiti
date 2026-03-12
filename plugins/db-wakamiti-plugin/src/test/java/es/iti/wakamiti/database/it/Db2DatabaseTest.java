eb854c213960a7f0213d00ef054b878963afe41d
Maria Galbis <mgalbis@iti.es>
2026-03-12 16:00:41 +0100

    test(db): update container setup to handle Docker npipe issues on Windows

--- plugins/db-wakamiti-plugin/src/test/java/es/iti/wakamiti/database/it/Db2DatabaseTest.java
+++ plugins/db-wakamiti-plugin/src/test/java/es/iti/wakamiti/database/it/Db2DatabaseTest.java
@@ -55,7 +55,7 @@ public class Db2DatabaseTest {
     @BeforeClass
     public static void setUp() {
         System.out.println("Creating container. Please, be patient... ");
-        container.start();
+        TestcontainersWindowsNpipe.startOrSkipOnWindowsNpipeFailure(container);
         System.out.println(message("\rContainer [Db2Container] started with [url={}, username={}, password={}]",
                 container.getJdbcUrl(), container.getUsername(), container.getPassword()));
     }
