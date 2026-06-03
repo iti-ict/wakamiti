0b22334f518852be54581fa7fef755e32d5ea641
Maria Galbis <mgalbis@iti.es>
2026-06-03 16:22:57 +0200

    feat(allure): add Allure reporter plugin with configuration support

---
+++ plugins/allure-wakamiti-plugin/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module es.iti.wakamiti.report.allure {
+
+    exports es.iti.wakamiti.report.allure;
+
+    requires org.slf4j;
+    requires iti.commons.jext;
+    requires es.iti.wakamiti.api;
+    requires com.fasterxml.jackson.databind;
+
+}
