7cbce6d6425f4e0303d31a55b83a9fc56c8fe115
Maria Galbis <mgalbis@iti.es>
2026-07-26 21:40:22 +0200

    chore: add copyright notice to multiple files

--- wakamiti-engine/wakamiti-junit/src/main/java/es/iti/wakamiti/junit/Profile.java
+++ wakamiti-engine/wakamiti-junit/src/main/java/es/iti/wakamiti/junit/Profile.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2022-2026 Instituto Tecnológico de Informática (ITI)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 package es.iti.wakamiti.junit;
 
 
